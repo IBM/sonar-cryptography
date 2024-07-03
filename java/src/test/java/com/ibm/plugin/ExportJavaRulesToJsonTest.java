9d8eeede98a88748d1e9743e745e01b22e09c043
Hugo Queinnec <hugo.queinnec@ibm.com>
2024-07-03 09:06:55 +0200

    move export
    Signed-off-by: Hugo Queinnec <hugo.queinnec@ibm.com>

--- java/src/test/java/com/ibm/plugin/ExportJavaRulesToJsonTest.java
+++ java/src/test/java/com/ibm/plugin/ExportJavaRulesToJsonTest.java
@@ -17,7 +17,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.ibm.plugin.rules;
+package com.ibm.plugin;
 
 import com.ibm.engine.serializer.ExportRules;
 import com.ibm.plugin.rules.detection.JavaDetectionRules;
