d88ed73064efcc83673a447ea141b04735ab28c9
Nicklas Körtge <nicklas.koertge1@ibm.com>
2024-08-01 09:23:52 +0200

    add Unknown node
    Signed-off-by: Nicklas Körtge <nicklas.koertge1@ibm.com>

--- mapper/src/main/java/com/ibm/mapper/mapper/jca/JcaAlgorithmMapper.java
+++ mapper/src/main/java/com/ibm/mapper/mapper/jca/JcaAlgorithmMapper.java
@@ -21,11 +21,13 @@ package com.ibm.mapper.mapper.jca;
 
 import com.ibm.mapper.mapper.IMapper;
 import com.ibm.mapper.model.INode;
+import com.ibm.mapper.model.Unknown;
 import com.ibm.mapper.utils.DetectionLocation;
-import java.util.List;
-import java.util.Optional;
+
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.List;
+import java.util.Optional;
 
 public class JcaAlgorithmMapper implements IMapper {
 
@@ -57,6 +59,7 @@ public class JcaAlgorithmMapper implements IMapper {
                 return asset;
             }
         }
-        return Optional.empty();
+
+        return Optional.of(new Unknown(str, detectionLocation));
     }
 }
