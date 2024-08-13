682ca9e8eff2793043503de1a62cb69db895ef5e
Hugo Queinnec <hugo.queinnec@ibm.com>
2024-08-13 15:45:13 +0200

    Grain model
    Signed-off-by: Hugo Queinnec <hugo.queinnec@ibm.com>

---
+++ mapper/src/main/java/com/ibm/mapper/model/algorithms/grain/Grainv1.java
@@ -0,0 +1,36 @@
+/*
+ * SonarQube Cryptography Plugin
+ * Copyright (C) 2024 IBM
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ibm.mapper.model.algorithms.grain;
+
+import com.ibm.mapper.model.ClassicalBitSecurityLevel;
+import com.ibm.mapper.model.KeyLength;
+import com.ibm.mapper.model.StreamCipher;
+import com.ibm.mapper.utils.DetectionLocation;
+import javax.annotation.Nonnull;
+
+public class Grainv1 extends Grain {
+    private static final String NAME = "Grain v1";
+
+    public Grainv1(@Nonnull DetectionLocation detectionLocation) {
+        super(NAME, StreamCipher.class, detectionLocation);
+        this.append(new KeyLength(80, detectionLocation));
+        this.append(new ClassicalBitSecurityLevel(80, detectionLocation));
+    }
+}
