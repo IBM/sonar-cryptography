51619620bc33033b14a45efe521d82123719e0fc
Hugo Queinnec <hugo.queinnec@ibm.com>
2024-08-20 15:42:21 +0200

    update BcCBCBlockCipherTest
    Signed-off-by: Hugo Queinnec <hugo.queinnec@ibm.com>

--- java/src/test/files/rules/detection/bc/blockcipher/BcCBCBlockCipherTestFile.java
+++ java/src/test/files/rules/detection/bc/blockcipher/BcCBCBlockCipherTestFile.java
@@ -6,8 +6,8 @@ import org.bouncycastle.crypto.params.KeyParameter;
 public class BcCBCBlockCipherTestFile {
     public void AESCipherCBCnoPad1(byte[] key) {
         // TODO: This detection should optimally not happen once AES has been detected as a child finding of CBCBlockCipher
-        org.bouncycastle.crypto.BlockCipher aes = new AESFastEngine(); //  Noncompliant {{AES}}
-        CBCBlockCipher cbc = new CBCBlockCipher(aes); //  Noncompliant {{CBC}}
+        org.bouncycastle.crypto.BlockCipher aes = new AESFastEngine(); //  Noncompliant {{AESFastEngine}}
+        CBCBlockCipher cbc = new CBCBlockCipher(aes); //  Noncompliant {{CBCBlockCipher}}
         KeyParameter kp = new KeyParameter(key);
         cbc.init(true, kp);
         return;
@@ -15,8 +15,8 @@ public class BcCBCBlockCipherTestFile {
 
     public void AESCipherCBCnoPad2(byte[] key) {
         // TODO: This detection should optimally not happen once AES has been detected as a child finding of CBCBlockCipher
-        org.bouncycastle.crypto.BlockCipher aes = AESEngine.newInstance(); //  Noncompliant {{AES}}
-        CBCBlockCipher cbc = CBCBlockCipher.newInstance(aes); //  Noncompliant {{CBC}}
+        org.bouncycastle.crypto.BlockCipher aes = AESEngine.newInstance(); //  Noncompliant {{AESEngine}}
+        CBCBlockCipher cbc = CBCBlockCipher.newInstance(aes); //  Noncompliant {{CBCBlockCipher}}
         KeyParameter kp = new KeyParameter(key);
         cbc.init(false, kp);
         return;
