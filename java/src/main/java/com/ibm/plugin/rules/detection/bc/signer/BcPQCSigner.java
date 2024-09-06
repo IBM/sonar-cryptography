2794f36068f0596d6d7e07383a6487b2b6dd59f1
Hugo Queinnec <hugo.queinnec@ibm.com>
2024-09-06 14:52:38 +0200

    fix
    Signed-off-by: Hugo Queinnec <hugo.queinnec@ibm.com>

--- java/src/main/java/com/ibm/plugin/rules/detection/bc/signer/BcPQCSigner.java
+++ java/src/main/java/com/ibm/plugin/rules/detection/bc/signer/BcPQCSigner.java
@@ -48,7 +48,7 @@ public final class BcPQCSigner {
                         .createDetectionRule()
                         .forObjectExactTypes("org.bouncycastle.pqc.crypto.DigestingMessageSigner")
                         .forConstructor()
-                        .shouldBeDetectedAs(new ValueActionFactory<>("DigestingMessage"))
+                        .shouldBeDetectedAs(new ValueActionFactory<>("DigestingMessageSigner"))
                         .withMethodParameter("org.bouncycastle.pqc.crypto.MessageSigner")
                         .addDependingDetectionRules(BcMessageSigner.rules())
                         .withMethodParameter("org.bouncycastle.crypto.Digest")
@@ -63,7 +63,8 @@ public final class BcPQCSigner {
                         .forObjectExactTypes(
                                 "org.bouncycastle.pqc.crypto.DigestingStateAwareMessageSigner")
                         .forConstructor()
-                        .shouldBeDetectedAs(new ValueActionFactory<>("DigestingStateAwareMessage"))
+                        .shouldBeDetectedAs(
+                                new ValueActionFactory<>("DigestingStateAwareMessageSigner"))
                         .withMethodParameter("org.bouncycastle.pqc.crypto.StateAwareMessageSigner")
                         .addDependingDetectionRules(BcStateAwareMessageSigner.rules())
                         .withMethodParameter("org.bouncycastle.crypto.Digest")
