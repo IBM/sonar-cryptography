00e800916481c601503ca296ebc681d3b86cea11
Hugo Queinnec <hugo.queinnec@ibm.com>
2024-09-16 08:57:24 +0200

    enable issue test
    Signed-off-by: Hugo Queinnec <hugo.queinnec@ibm.com>

--- java/src/test/files/rules/issues/NextParameterDependingRulesTestFile.java
+++ java/src/test/files/rules/issues/NextParameterDependingRulesTestFile.java
@@ -5,15 +5,15 @@ public class NextParameterDependingRulesTestFile {
 
     /* Does not work: AES is not set as a child of GCM */
     public GCMBlockCipher test1() {
-        GCMBlockCipher blockCipher = GCMBlockCipher.newInstance(AESEngine.newInstance()); // Noncompliant {{GCM}}
-        // Noncompliant@-1 {{AES}}
+        GCMBlockCipher blockCipher = GCMBlockCipher.newInstance(AESEngine.newInstance()); // Noncompliant {{GCMBlockCipher}}
+        // Noncompliant@-1 {{AESEngine}}
         return blockCipher;
     }
 
     /* Works: AES is set as a child of GCM */
     public GCMBlockCipher test2() { 
-        GCMBlockCipher blockCipher = new GCMBlockCipher(new AESEngine()); // Noncompliant {{GCM}}
-        // Noncompliant@-1 {{AES}}
+        GCMBlockCipher blockCipher = new GCMBlockCipher(new AESEngine()); // Noncompliant {{GCMBlockCipher}}
+        // Noncompliant@-1 {{AESEngine}}
         return blockCipher;
     }
 }
