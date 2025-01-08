0a3d3ee6b4b1d9426a8f5d272a5471cf70bc556e
Nicklas Körtge <nicklas.koertge@protonmail.com>
2025-01-08 11:23:16 +0100

    update dependencies, update readme, small code updates (#202)
    Signed-off-by: Nicklas Körtge <nicklas.koertge1@ibm.com>

--- mapper/src/main/java/com/ibm/mapper/mapper/ssl/json/JsonCipherSuites.java
+++ mapper/src/main/java/com/ibm/mapper/mapper/ssl/json/JsonCipherSuites.java
@@ -1,6 +1,6 @@
 /*
  * SonarQube Cryptography Plugin
- * Copyright (C) 2024 IBM
+ * Copyright (C) 2025 IBM
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
